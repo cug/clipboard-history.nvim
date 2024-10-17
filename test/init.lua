c8083d90dfed45529b95dd4eedd28b20e7443b9a
Anirudh Roy <82973472+royanirudd@users.noreply.github.com>
2024-10-17 14:32:23 +0530

    Delete test directory

--- test/init.lua
+++
@@ -1,6 +0,0 @@
--- test_nvim_config/init.lua
-vim.opt.runtimepath:append("../clipboard-history.nvim")
-
-require("clipboard-history").setup()
-
--- Optional: add any other settings you want for testing
